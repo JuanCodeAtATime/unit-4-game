61a8964302a3dd46fd48fbb32d3b18a5bae117c5
Juan Rivera <jrivera@primalplatoon.com>
2019-10-12 12:33:49 -0400

    Fixed the reset function of the crystal sums.  Still having issues displaying the matched scores, but overall, I'm satisfied with the current product.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,66 +1,72 @@
 $(document).ready(function () {
 
+
     //This toggles the game instructions button to hide this lengthy box
-    $(".btn").on('click', function () {
+    $("#btnGame").on('click', function () {
         $('.hideMe').slideToggle(1000);
 
     });
-    // Initial Variables
+    // Initializing Variables
     let computerNumber = Math.floor(Math.random() * 120 + 21);
     //Logging computer's number to console for troubleshooting if necessary
     console.log(computerNumber);
 
-    let wins = 1;
-    let losses = 1;
-    let userTotal = 0;
+    let wins = 0;
+    let losses = 0;
+    let crystalSum = 0;
     let redRuby = Math.floor(Math.random() * 12) + 1;
     let diamond = Math.floor(Math.random() * 12) + 1;
     let yellowSapphire = Math.floor(Math.random() * 12) + 1;
     let greenEmerald = Math.floor(Math.random() * 12) + 1;
-    let crystalSum = 0;
-
 
     function updateLosses() {
+        losses++
         document.querySelector("#losses").innerHTML = "Losses: " + losses;
     }
 
     function updateWins() {
+        wins++
         document.querySelector("#wins").innerHTML = "Wins: " + wins;
     }
 
-    // function resetComputerNumber() {
-    //     document.querySelector(".score").innerHTML = Math.floor(Math.random() * 120 + 21);
-    // }
+    function resetComputerNumber() {
+        computerNumber = Math.floor(Math.random() * 120 + 21);
+        document.querySelector(".score").innerHTML = computerNumber;
+    }
 
-    function crystalRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-
-
+    function resetCrystalSum() {
+        crystalSum = 0;
+        document.getElementById("placeholder").innerHTML = crystalSum;
 
     }
+
+
     function crystalsSum(sum) {
         crystalSum += sum
         console.log(crystalSum);
         $("#placeholder").html(crystalSum);
 
         if (crystalSum === computerNumber) {
+
             alert("You win!");
             updateWins();
-            // resetComputerNumber();
-
+            resetComputerNumber();
+            resetCrystalSum();
+        }
+        else if (crystalSum >= computerNumber) {
+            // let temp = crystalSum;
+            updateLosses();
+            resetComputerNumber();
+            alert("You lose!! " + crystalSum + ' is your guess');
+            resetCrystalSum();
         }
 
-        else if (crystalSum > computerNumber) {
-            alert("You lose!!");
-            updateLosses()
-            losses++;
-            // resetComputerNumber();
-
-        }
 
     }
     //This displays the computer-generated random number in the html
     $(".score").text(computerNumber);
+
+    //Using jQuery functions to increment scores in each crystal
 
     $('#redRuby').click(function () {
         crystalsSum(redRuby);
@@ -79,37 +85,11 @@ $(document).ready(function () {
     })
 
 
-    // if (crystalSum === computerNumber) {
-    //     alert("You win!");
-    // }
-
-    // else if (crystalSum > computerNumber) {
-    //     alert("You lose!!");
-    //     losses++;
-    // }
-
-
 
 });
 
 
-
-
-//This alerts User when his score matches the computer's Number
-
-// if (crystalSum === computerNumber) {
-//     alert("You win!");
-// }
-
-// else if (crystalSum > computerNumber) {
-//     alert("You lose!!");
-//     losses++;
-// }
-
-//     //resetGame //create function to reset game without resetting Wins/Losses variable
-
-
-// //Hover functions over each ruby setup for alerts for troubleshooting.  
+// //Hover functions over each ruby set up to alert for troubleshooting.  
 // $("#redRuby").hover(function () {
 //     console.log(redRuby);
 // });
